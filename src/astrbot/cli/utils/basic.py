147894c17823e1812f3803d845da785209c280fb
LIghtJUNction <LIghtJUNction.me@gmail.com>
2025-07-18 21:09:44 +0800

    Update basic.py

--- src/astrbot/cli/utils/basic.py
+++ src/astrbot/cli/utils/basic.py
@@ -18,7 +18,7 @@ def check_astrbot_root(path: str | Path) -> bool:
 def get_astrbot_root() -> Path:
     """获取Astrbot根目录路径"""
     if ROOT:=os.getenv("ASTRBOT_ROOT"):
-        return ROOT
+        return Path(ROOT)
     else:
         return Path.cwd()
 
